d0818fed6a0adddb93ba7865acb7000ceafcd1a4
Alex Stepanov <stepanov.hw@gmail.com>
2023-08-28 14:31:33 -0600

    Stub out the starter components, import all react and react-router, import the Routes and Route component from react-router, setup our the routes, test all routes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+// IMPORT COMPONENTS
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Route, Routes } from "react-router-dom";
+
+// IMPORT PAGES
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
 
 export default App;
-
 
